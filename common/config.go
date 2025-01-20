1bc134f43c7f2b1b2ad99a9797892f933717bf25
Luka <0x582f1fb0@gmail.com>
2025-01-21 01:53:51 +0800

    init

--- common/config.go
+++ common/config.go
@@ -11,12 +11,15 @@ import (
 )
 
 func GetHosts(szHostsFile string) []string {
-	szFuncName := "GetHosts"
-	szHosts := []string{}
-	file, err := os.Open(szHostsFile)
-	if err != nil {
-		logger.ERROR("<" + szFuncName + "> -> " + err.Error())
+	if _, err := os.Stat(szHostsFile); err != nil {
+		szFile, _ := os.Create(szHostsFile)
+		defer szFile.Close()
+		logger.Log(szHostsFile, "192.168.1.1")
+		logger.Log(szHostsFile, "192.168.1.2")
 	}
+
+	szHosts := []string{}
+	file, _ := os.Open(szHostsFile)
 	defer file.Close()
 	fileScanner := bufio.NewScanner(file)
 	// fileScanner.Split(bufio.ScanLines)
@@ -30,6 +33,21 @@ func GetHosts(szHostsFile string) []string {
 }
 
 func GetMailConfig(szConfigFile string) map[string]string {
+	if _, err := os.Stat(szConfigFile); err != nil {
+		szFile, _ := os.Create(szConfigFile)
+		defer szFile.Close()
+		logger.Log(szConfigFile, "[SMTP]")
+		logger.Log(szConfigFile, "server = smtp.gmail.com")
+		logger.Log(szConfigFile, "port = 587")
+		logger.Log(szConfigFile, "sender = ")
+		logger.Log(szConfigFile, "password = ")
+		logger.Log(szConfigFile, "receiver = ")
+		logger.Log(szConfigFile, "")
+		logger.Log(szConfigFile, "[Mail]")
+		logger.Log(szConfigFile, "subject = \"設備斷線告警\"")
+		logger.Log(szConfigFile, "body = \"設備無回應，請檢查網絡連接！!\"")
+	}
+
 	szResult := map[string]string{}
 	cfg, err := ini.Load(szConfigFile)
 	if err != nil {
